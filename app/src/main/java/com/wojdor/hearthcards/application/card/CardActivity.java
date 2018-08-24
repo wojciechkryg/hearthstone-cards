1b767de6d18c19288dda8582838b535930f288c5
Wojciech Kryg <krygwojciech@gmail.com>
2018-08-24 18:51:57 +0200

    Add card details

--- app/src/main/java/com/wojdor/hearthcards/application/card/CardActivity.java
+++ app/src/main/java/com/wojdor/hearthcards/application/card/CardActivity.java
@@ -3,7 +3,10 @@ package com.wojdor.hearthcards.application.card;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -20,8 +23,22 @@ public class CardActivity extends BaseActivity {
 
     public static final String CARD_ID_EXTRA = "CARD_ID_EXTRA";
 
+    @BindView(R.id.cardToolbar)
+    Toolbar cardToolbar;
     @BindView(R.id.cardCardIv)
     ImageView cardCardIv;
+    @BindView(R.id.cardCardName)
+    TextView cardCardName;
+    @BindView(R.id.cardCardSet)
+    TextView cardCardSet;
+    @BindView(R.id.cardCardRarity)
+    TextView cardCardRarity;
+    @BindView(R.id.cardCardClass)
+    TextView cardCardClass;
+    @BindView(R.id.cardCardFlavor)
+    TextView cardCardFlavor;
+    @BindView(R.id.cardCardArtist)
+    TextView cardCardArtist;
 
     private CardViewModel viewModel;
 
@@ -35,10 +52,25 @@ public class CardActivity extends BaseActivity {
     }
 
     private void initComponents() {
+        initToolbar();
+        initCardDetails();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(cardToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initCardDetails() {
         String cardId = getIntent().getStringExtra(CARD_ID_EXTRA);
         viewModel.getCardImage(cardId).observe(this, this::loadCardImage);
         viewModel.getCardByCardId(cardId).observe(this, card -> {
-            //TODO: Show card info
+            cardCardName.setText(card.getName());
+            cardCardSet.setText(card.getSet());
+            cardCardRarity.setText(card.getRarity());
+            cardCardClass.setText(card.getClassName());
+            cardCardFlavor.setText(Html.fromHtml(card.getFlavorText()));
+            cardCardArtist.setText(card.getArtist());
         });
     }
 
@@ -48,5 +80,11 @@ public class CardActivity extends BaseActivity {
                 .apply(new RequestOptions().placeholder(R.drawable.ic_card).error(R.drawable.ic_card))
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(cardCardIv);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
