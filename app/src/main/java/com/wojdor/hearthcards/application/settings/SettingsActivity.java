1bd2b1b28995eaf5524fc3df578e898e479c9789
Wojciech Kryg <krygwojciech@gmail.com>
2018-08-25 18:19:26 +0200

    Add back icon to settings screen

--- app/src/main/java/com/wojdor/hearthcards/application/settings/SettingsActivity.java
+++ app/src/main/java/com/wojdor/hearthcards/application/settings/SettingsActivity.java
@@ -2,15 +2,35 @@ package com.wojdor.hearthcards.application.settings;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 
 import com.wojdor.hearthcards.R;
 import com.wojdor.hearthcards.application.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SettingsActivity extends BaseActivity {
+
+    @BindView(R.id.settingsToolbar)
+    Toolbar settingsToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(settingsToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
