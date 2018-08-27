07f4387ac77372bc8b00904136b695258e9eac99
Wojciech Kryg <krygwojciech@gmail.com>
2018-08-27 18:18:47 +0200

    Fix parsing html

--- app/src/main/java/com/wojdor/hearthcards/application/util/HtmlParser.java
+++ app/src/main/java/com/wojdor/hearthcards/application/util/HtmlParser.java
@@ -7,18 +7,20 @@ public class HtmlParser {
 
     private static final String EMPTY = "";
     private static final String SPACE = " ";
-    private static final String UNDERLINE = "_";
     private static final String ENTER = "\n";
     private static final String ENTER_SIGN = "\\n";
     private static final String ENTER_HTML = "<br>";
     private static final String DOLLAR_SIGN = "$";
+    private static final String UNDERLINE = "_";
+    private static final String WEIRD_X_SYMBOL = "[x]";
 
     public Spanned asHtml(String text) {
         if (text == null) return Html.fromHtml(EMPTY);
         String editedText = text
                 .replace(ENTER_SIGN, ENTER_HTML)
                 .replace(DOLLAR_SIGN, EMPTY)
-                .replace(UNDERLINE, SPACE);
+                .replace(UNDERLINE, SPACE)
+                .replace(WEIRD_X_SYMBOL, EMPTY);
         return Html.fromHtml(editedText);
     }
 
@@ -27,6 +29,7 @@ public class HtmlParser {
         return Html.fromHtml(text).toString()
                 .replace(ENTER_SIGN, ENTER)
                 .replace(DOLLAR_SIGN, EMPTY)
-                .replace(UNDERLINE, SPACE);
+                .replace(UNDERLINE, SPACE)
+                .replace(WEIRD_X_SYMBOL, EMPTY);
     }
 }
