4484dd9987f61fafc4ecebada61d661420e87d09
Wojciech Kryg <krygwojciech@gmail.com>
2018-09-07 16:23:35 +0200

    Fix versions and refactor

--- app/src/main/java/com/wojdor/hearthstonecards/application/classcards/ClassCardViewHolder.kt
+++ app/src/main/java/com/wojdor/hearthstonecards/application/classcards/ClassCardViewHolder.kt
@@ -2,7 +2,6 @@ package com.wojdor.hearthstonecards.application.classcards
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
 
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -10,14 +9,22 @@ import com.bumptech.glide.request.RequestOptions
 import com.wojdor.hearthstonecards.R
 import com.wojdor.hearthstonecards.application.util.FileStorage
 import com.wojdor.hearthstonecards.domain.Card
+import kotlinx.android.synthetic.main.item_card.view.*
 
 class ClassCardViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
-    private val itemCardCardIv: ImageView = view.findViewById(R.id.itemCardCardIv)
     private val fileStorage: FileStorage = FileStorage(view.context)
 
     fun bind(card: Card, onCardClick: (Card) -> Unit) {
-        Glide.with(itemCardCardIv.context)
+        with(view) {
+            loadCardImage(card)
+            itemCardCardIv.contentDescription = card.name
+            setOnClickListener { _ -> onCardClick(card) }
+        }
+    }
+
+    private fun View.loadCardImage(card: Card) {
+        Glide.with(context)
                 .load(fileStorage.get(card.cardId))
                 .apply(RequestOptions()
                         .placeholder(R.drawable.ic_card)
@@ -25,7 +32,5 @@ class ClassCardViewHolder(private val view: View) : RecyclerView.ViewHolder(view
                         .diskCacheStrategy(DiskCacheStrategy.NONE)
                         .skipMemoryCache(true))
                 .into(itemCardCardIv)
-        itemCardCardIv.contentDescription = card.name
-        view.setOnClickListener { _ -> onCardClick(card) }
     }
 }
